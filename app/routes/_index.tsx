3dfca388c77228f06ece5e731755266221c823b4
satishnvrn <nvrn.satish@gmail.com>
2023-05-13 20:01:59 +0530

    Styling the form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,11 +8,14 @@ export default function Index() {
   return (
     <div>
       <div className="banner"/>
-      <div className="mt-12.5 flex flex-col items-center">
-        <form>
-          <div className="flex items-center gap-3">
+      <div className="mt-14 flex flex-col items-center">
+        <form className="flex flex-col justify-center">
+          <div className="flex items-center gap-3 form-group">
+            <label htmlFor="token">Enter SOL Token: </label>
             <input type="text" id="token" name="token" placeholder="SOL Token ..."/>
           </div>
+          <br />
+          <input type="submit" className="bg-[#4CAF50] text-white py-3 px-5 border-none rounded cursor-pointer text-base"/>
         </form>
       </div>
     </div>
